9a0c70fd6df824b9c99c1737bcd3c96a364143e5
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-08-02 02:00:52 +0000

    [Update]ページの閲覧数のカウントと表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_01_230222) do
+ActiveRecord::Schema.define(version: 2023_08_02_013159) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -82,6 +82,15 @@ ActiveRecord::Schema.define(version: 2023_08_01_230222) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "read_counts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_read_counts_on_book_id"
+    t.index ["user_id"], name: "index_read_counts_on_user_id"
+  end
+
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
@@ -116,5 +125,7 @@ ActiveRecord::Schema.define(version: 2023_08_01_230222) do
   add_foreign_key "entries", "users"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "read_counts", "books"
+  add_foreign_key "read_counts", "users"
   add_foreign_key "rooms", "users"
 end
